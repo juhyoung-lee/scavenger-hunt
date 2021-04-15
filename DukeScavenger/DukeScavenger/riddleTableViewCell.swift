a5e7f935aac5a4e3f38f034ecbadf1e3d3f8b9df
Nicolas Aldana <na158@duke.edu>
2021-04-15 02:50:30 -0400

    added database access functions

--- DukeScavenger/DukeScavenger/riddleTableViewCell.swift
+++ DukeScavenger/DukeScavenger/riddleTableViewCell.swift
@@ -11,10 +11,14 @@ class riddleTableViewCell: UITableViewCell {
 
     
     @IBOutlet weak var riddleNum: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+
+
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
